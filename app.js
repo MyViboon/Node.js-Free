64f112ead423d3abeb4e31591d59d839e9a71188
MyViboon <bakpu11@gmail.com>
2022-04-29 17:40:12 +0700

    12

--- app.js
+++ app.js
@@ -4,37 +4,19 @@ const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const products = require("./data/products.json");
-const productsRouter = express.Router();
 const PORT = process.env.PORT || 5000;
+const productsRouter = require("./src/router/productsRouter");
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, "/public/")));
-
 app.set('views', "./src/views");
 app.set('view engine', "ejs");
-
-productsRouter.route("/").get((req,res) => {
-    res.render("products",{
-        products,
-    }
-    );
-});
-
-productsRouter.route("/:id").get((req,res) => {
-    const id = req.params.id;
-    res.render("product", {
-        product : products[id],
-    })
-});
 
 app.use("/products", productsRouter);
 
 app.get("/", (req,res) =>{
-
     // res.render('index', {username: "Viboon 555+", customer: ["dog555", "cat", "pick"]});
     res.render('index');
-
 })
 
 app.listen(PORT, ()=>{
