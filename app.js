92b9f75c6c3be2829b4f941e4c910acceb786315
MyViboon <bakpu11@gmail.com>
2022-04-29 16:30:18 +0700

    12

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const app = express();
 const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const products = require("./data/products.json");
 const productsRouter = express.Router();
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(morgan('combined'));
@@ -15,12 +16,7 @@ app.set('view engine', "ejs");
 
 productsRouter.route("/").get((req,res) => {
     res.render("products", {
-        products: [
-            {productTitle:"น้ำยาล้างจาน", ProductDescription:'น้ำยาล้างจานสูตร 1', productPrice: 45},
-            {productTitle:"น้ำยาล้างจาน 2", ProductDescription:'น้ำยาล้างจานสูตร 2', productPrice: 65},
-            {productTitle:"น้ำยาล้างจาน 3", ProductDescription:'น้ำยาล้างจานสูตร 3', productPrice: 85},
-            {productTitle:"น้ำยาล้างจาน 4", ProductDescription:'น้ำยาล้างจานสูตร 4', productPrice: 35}
-        ],
+        products,
     });
 });
 
