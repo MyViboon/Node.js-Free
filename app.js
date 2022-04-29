2213e7d7a458797dc14f4ceb60b7d1dc02e19fc1
MyViboon <bakpu11@gmail.com>
2022-04-29 16:31:51 +0700

    Update app.js

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.set('views', "./src/views");
 app.set('view engine', "ejs");
 
 productsRouter.route("/").get((req,res) => {
-    res.render("products", {
+    res.render("products", 
         products,
-    });
+    );
 });
 
 productsRouter.route("/1").get((req,res) => {
